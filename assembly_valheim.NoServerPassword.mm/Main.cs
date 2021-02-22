70eedad5943c7c9e9417ab4e598bb04edafb6708
Artem Gurevich <gurevichae@gmail.com>
2021-02-22 16:02:07 +0300

    Added NoServerPassword sample mod

---
+++ assembly_valheim.NoServerPassword.mm/Main.cs
@@ -0,0 +1,10 @@
+﻿using MonoMod;
+
+public class patch_FejdStartup : FejdStartup
+{
+    [MonoModReplace]
+    private bool IsPublicPasswordValid(string password, World world)
+    {
+        return true;
+    }
+}
