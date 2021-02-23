025e486dd5e467f2c5699724c62c33969ff16f8a
Artem Gurevich <gurevichae@gmail.com>
2021-02-23 22:39:34 +0300

    SharedMap prototype

--- assembly_valheim.SharedMap.mm/Main.cs
+++ assembly_valheim.SharedMap.mm/Main.cs
@@ -1,97 +1,121 @@
-﻿using MonoMod;
+﻿using System.Collections.Generic;
+using MonoMod;
 using UnityEngine;
 
 #pragma warning disable CS0626
 #pragma warning disable CS0649
 
-class patch_Game : Game
-{
-    private extern void orig_Start();
-
-    private void Start()
-    {
-        orig_Start();
-        Debug.Log($"{nameof(Game)}: Start IsServer={ZNet.instance.IsServer()}");
-
-        ZRoutedRpc.instance.Register("ValheimUtils_MapDataRequest", RPC_ValheimUtils_MapDataRequest);
-        ZRoutedRpc.instance.Register<long>("ValheimUtils_MapDataSend", RPC_ValheimUtils_MapDataSend);
-    }
-
-    private void RPC_ValheimUtils_MapDataRequest(long sender)
-    {
-        Debug.Log($"{nameof(Game)}: RPC_ValheimUtils_MapDataRequest Sender={sender}");
-
-        foreach (var peer in ZNet.instance.GetConnectedPeers())
-        {
-            Debug.Log($"{nameof(Game)}: RPC_ValheimUtils_MapDataRequest ConnectedPeer={peer.m_uid}");
-            ZRoutedRpc.instance.InvokeRoutedRPC(peer.m_uid, "ValheimUtils_MapDataSend", sender);
-        }
-    }
-
-    private void RPC_ValheimUtils_MapDataSend(long sender, long requesterId)
-    {
-        Debug.Log($"{nameof(Game)}: RPC_ValheimUtils_MapDataSend Sender={sender} RequesterId={requesterId}");
-
-        Minimap.instance.SaveMapData();
-
-        ZRoutedRpc.instance.InvokeRoutedRPC(
-            requesterId,
-            "ValheimUtils_MapDataResponse",
-            new ZPackage(GetPlayerProfile().GetMapData())
-        );
-    }
-}
-
 class patch_Player : Player
 {
-    private extern void orig_OnSpawned();
+    private bool m_enterGame = true;
+
+    public extern void orig_OnSpawned();
 
     public new void OnSpawned()
     {
-        orig_OnSpawned();
         Debug.Log($"{nameof(Player)}: OnSpawned PlayerName={GetPlayerName()}");
+        orig_OnSpawned();
 
-        if (m_nview.IsOwner())
+        if (m_enterGame)
         {
-            ZRoutedRpc.instance.InvokeRoutedRPC("ValheimUtils_MapDataRequest");
+            foreach (var mapScanline in ((patch_Minimap) Minimap.instance).SharedMap_GetMapScanlines())
+            {
+                ZRoutedRpc.instance.InvokeRoutedRPC("SharedMap_Update", mapScanline);
+            }
+
+            m_enterGame = false;
         }
     }
 }
 
 class patch_Minimap : Minimap
 {
-    // expose Minimap private fields
+    // expose Minimap fields
     [MonoModIgnore] private Texture2D m_fogTexture;
     [MonoModIgnore] private bool[] m_explored;
 
-    // expose Minimap private methods
     [MonoModIgnore]
     private extern bool Explore(int x, int y);
 
-    // patch Minimap methods
     private extern void orig_Start();
 
     private void Start()
     {
+        Debug.Log($"{nameof(Minimap)}: Start IsServer={ZNet.instance.IsServer()}");
         orig_Start();
-        Debug.Log($"{nameof(Minimap)}: Start");
 
-        ZRoutedRpc.instance.Register<ZPackage>("ValheimUtils_MapDataResponse", RPC_ValheimUtils_MapDataResponse);
+        if (ZNet.instance.IsServer())
+        {
+            ZRoutedRpc.instance.Register<ZPackage>("SharedMap_Update", RPC_SharedMap_Update);
+        }
+        else
+        {
+            ZRoutedRpc.instance.Register<ZPackage>("SharedMap_Apply", RPC_SharedMap_Apply);
+        }
     }
 
-    // new Minimap methods
-    public void RPC_ValheimUtils_MapDataResponse(long sender, ZPackage mapData)
+    public IEnumerable<ZPackage> SharedMap_GetMapScanlines()
     {
-        Debug.Log($"{nameof(Minimap)}: RPC_ValheimUtils_MapDataResponse Sender={sender} MapDataSize={mapData.Size()}");
-
-        int mapVersion = mapData.ReadInt();
-        int mapSize = mapData.ReadInt();
-
-        for (int index = 0; index < m_explored.Length; ++index)
+        for (int y = 0; y < m_textureSize; ++y)
         {
-            if (mapData.ReadBool())
+            var scanline = new ZPackage();
+
+            scanline.Write(m_textureSize);
+            scanline.Write(y);
+
+            for (int x = 0; x < m_textureSize; ++x)
             {
-                Explore(index % mapSize, index / mapSize);
+                scanline.Write(m_explored[y * m_textureSize + x]);
+            }
+
+            yield return scanline;
+        }
+    }
+
+    private void RPC_SharedMap_Update(long sender, ZPackage mapScanline)
+    {
+        Debug.Log($"{nameof(Minimap)}: RPC_SharedMap_Update Sender={sender} MapScanlineSize={mapScanline.Size()}");
+
+
+        int mapSize = mapScanline.ReadInt();
+        if (m_textureSize != mapSize)
+            return;
+
+        var y = mapScanline.ReadInt();
+
+        var tempScanline = new ZPackage();
+        tempScanline.Write(mapSize);
+        tempScanline.Write(y);
+
+        for (int x = 0; x < m_textureSize; ++x)
+        {
+            if (mapScanline.ReadBool())
+            {
+                Debug.Log($"{nameof(Minimap)}: SharedMap Explore({x}, {y})");
+                m_explored[y * m_textureSize + x] = true;
+            }
+
+            tempScanline.Write(m_explored[y * m_textureSize + x]);
+        }
+
+        ZRoutedRpc.instance.InvokeRoutedRPC(ZRoutedRpc.Everybody, "SharedMap_Apply", tempScanline);
+    }
+
+    private void RPC_SharedMap_Apply(long sender, ZPackage mapScanline)
+    {
+        Debug.Log($"{nameof(Minimap)}: RPC_SharedMap_Apply Sender={sender} MapScanlineSize={mapScanline.Size()}");
+
+        int mapSize = mapScanline.ReadInt();
+        if (m_textureSize != mapSize)
+            return;
+
+        var y = mapScanline.ReadInt();
+        for (int x = 0; x < m_textureSize; ++x)
+        {
+            if (mapScanline.ReadBool())
+            {
+                Debug.Log($"Explore({x}, {y})");
+                Explore(x, y);
             }
         }
 
