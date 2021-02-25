710c8ce79209005eb5cb57c27df7ca944d9dd5e3
Artem Gurevich <gurevichae@gmail.com>
2021-02-25 03:03:34 +0300

    Moved dependencies and added basic configuration.

---
+++ assembly_valheim.Config.mm/Main.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using BepInEx;
+using BepInEx.Configuration;
+using MonoMod;
+
+#pragma warning disable CS0626
+#pragma warning disable CS0649
+
+namespace ValheimConfig
+{
+    [MonoModPatch("global::WorldGenerator")]
+    public class ModConfig
+    {
+        public static ConfigFile Config;
+
+        // ExtendedStorage
+        public static ConfigEntry<bool> ExtendedStorageEnabled;
+
+        private extern void orig_ctor(World world);
+
+        [MonoModConstructor]
+        private void ctor(World world)
+        {
+            Config = new ConfigFile(Path.Combine(Paths.ConfigPath, "com.glcoder.ValheimUtils.cfg"), true);
+
+            ExtendedStorageEnabled = Config.Bind("ExtendedStorage", "Enabled", false,
+                "Enable ExtendedStorage mod."
+            );
+
+            orig_ctor(world);
+        }
+    }
+}
---
+++ assembly_valheim.ExtendedStorage.mm/Main.cs
@@ -0,0 +1,25 @@
+﻿using MonoMod;
+using ValheimConfig;
+
+#pragma warning disable CS0626
+#pragma warning disable CS0649
+
+namespace ValheimExtendedStorage
+{
+    [MonoModPatch("global::Container")]
+    class ModContainer : Container
+    {
+        private extern void orig_Awake();
+
+        private void Awake()
+        {
+            if (ModConfig.ExtendedStorageEnabled.Value)
+            {
+                m_width = m_width + 1;
+                m_height = m_height + 1;
+            }
+
+            orig_Awake();
+        }
+    }
+}
